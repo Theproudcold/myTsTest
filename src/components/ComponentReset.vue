19a14cc405cad01ba60dffc16ae236efcd0c73e2
jiuyan9377 <2297006922@qq.com>
2024-11-28 10:45:09 +0800

    vue组件状态重置

---
+++ src/components/ComponentReset.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex gap-20">
+    <span>a: {{ state1.a }}</span>
+    <span>b: {{ state1.b }}</span>
+    <span>c: {{ state1.c }}</span>
+    <span>d: {{ state1.d }}</span>
+    <el-space>
+      <el-button
+        type="primary"
+        @click="state1.a++"
+        >a++</el-button
+      >
+      <el-button
+        type="primary"
+        @click="state1.b++"
+        >b++</el-button
+      >
+      <el-button
+        type="primary"
+        @click="state1.c.push(1)"
+        >c push</el-button
+      >
+      <el-button
+        type="primary"
+        @click="state1.d = 1"
+        >d add</el-button
+      >
+      <el-button @click="reset">重置</el-button>
+    </el-space>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  useResetTableRefFn,
+  useResetTableRef,
+  useResetTableReactive,
+} from "@/hooks/useResetTable";
+// const {state1,reset} = useResetTableRefFn(() => ({
+//   a: 0,
+//   b: 0,
+//   c: [1, 2, 3],
+// }));
+// const {state1,reset} = useResetTableRef({
+//   a: 0,
+//   b: 0,
+//   c: [1, 2, 3],
+// });
+const [state1, reset] = useResetTableReactive({
+  a: 0,
+  b: 0,
+  c: [1, 2, 3],
+});
+</script>
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  flex-direction: column;
+}
+.gap-20 {
+  gap: 20px;
+}
+</style>
---
+++ src/hooks/useResetTable.ts
@@ -0,0 +1,44 @@
+import { reactive, ref } from "vue";
+export function useResetTableRefFn<T>(cb: () => T) {
+  const state = ref(cb());
+  const reset = () => {
+    state.value = cb();
+  };
+  return {
+    state,
+    reset,
+  };
+}
+/**
+ * 深拷贝
+ * @param value 需要拷贝的值
+ * @return 拷贝后的值
+ */
+function defaultClone(value: any) {
+  if (value === null || typeof value !== "object") {
+    return value;
+  }
+  return JSON.parse(JSON.stringify(value));
+}
+export function useResetTableRef<T>(value: T, clone = defaultClone) {
+  const initialValue = clone(value);
+  const state = ref(value);
+  const reset = () => {
+    state.value = clone(initialValue);
+  };
+  return {
+    state,
+    reset,
+  };
+}
+export function useResetTableReactive<T extends object>(
+  value: T,
+  clone = defaultClone
+) {
+  const state = reactive(clone(value)) as T;
+  const reset = () => {
+    Object.keys(state).forEach(key => delete state[key as keyof T]);
+    Object.assign(state, clone(value));
+  };
+  return [state, reset];
+}
